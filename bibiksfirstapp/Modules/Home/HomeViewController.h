cb7fb9c8d80f53926a86043ce125cee81bbe0915
biancaalt <biancaaltg@gmail.com>
2016-05-09 14:21:20 -0300

    inicio

---
+++ bibiksfirstapp/Modules/Home/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  bibiksfirstapp
+//
+//  Created by Tqt iOS on 5/9/2016.
+//  Copyright (c) 2016 Taqtile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ bibiksfirstapp/Supporting Files/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  bibiksfirstapp
+//
+//  Created by Tqt iOS on 5/9/2016.
+//  Copyright (c) 2016 Taqtile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT NSString * const kMyStringConstant;
+FOUNDATION_EXPORT int const kMyIntConstant;
+FOUNDATION_EXPORT BOOL const kBooleanConstant;
+
